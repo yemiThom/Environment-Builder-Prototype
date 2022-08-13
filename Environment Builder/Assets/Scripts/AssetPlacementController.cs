1da8247e92d74537fda88e4fd303929f54b07e74
Adeyemi Thompson <38887653+yemiThom@users.noreply.github.com>
2022-08-13 16:46:53 +0100

    Imported Low Poly 3D Assets

---
+++ Environment Builder/Assets/Scripts/AssetPlacementController.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AssetPlacementController : MonoBehaviour
+{
+    [SerializeField] GameObject[] _placeableAssetPrefabs;
+
+    
+
+    public void HandleAssetPlacement(int assetToPlace)
+    {
+
+    }
+}
